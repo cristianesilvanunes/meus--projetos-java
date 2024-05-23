f79f2956aa47ff947000caccc461e37e6b6e929c
cristianesilvanunes <eebmp.csn@educacao.arcos.mg.gov.br>
2024-05-23 20:50:23 -0300

    aula

---
+++ main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class main{
+    
+}
+
+public class VerificarObrigVoto {
+    public static void main(String[] args) {
+        int idade;
+        System.out.print("Digite sua idade: ");
+        idade=
+    }
+
+}
