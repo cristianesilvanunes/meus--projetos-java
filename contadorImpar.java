325ef62c946efae886df929c44c40d90c6725320
cristianesilvanunes <eebmp.csn@educacao.arcos.mg.gov.br>
2024-06-03 22:06:17 -0300

    0305

--- contadorImpar.java
+++ contadorImpar.java
@@ -1,30 +1,12 @@
 public class contadorImpar {
-    
-}public class ContadorImpar {
     public static void main(String[] args) {
-        // Inicializa a contagem de números ímpares
-        int contador = 0;
-        
-        // StringBuilder para armazenar os números ímpares
-        StringBuilder numerosImpares = new StringBuilder("São números ímpares: ");
-        
-        // Loop para encontrar e exibir números ímpares de 1 até 100
-        for (int i = 1; i <= 100; i++) {
-            if (i % 2 != 0) { // Verifica se o número é ímpar
-                numerosImpares.append(i).append(" - ");
-                contador++; // Incrementa o contador de números ímpares
+        int quantidade = 0;
+                for (int i = 1; i<100; i++) {
+                    if (i % 2 == 1) {
+                    System.out.print(" " + i + "  ||");
+                    quantidade++;
+                    }
+                }
+                System.out.println("\nExistem " + quantidade + " numeros impares de 0 a 100");
             }
-        }
-        
-        // Remove o último " - " desnecessário
-        if (numerosImpares.length() > 0) {
-            numerosImpares.setLength(numerosImpares.length() - 3);
-        }
-        
-        // Exibe os números ímpares
-        System.out.println(numerosImpares.toString());
-        
-        // Exibe a quantidade de números ímpares
-        System.out.println("Existem: " + contador + " números ímpares de 1 até 100.");
-    }
-}
+         } 
