01157723e9823e80c1fde80fe21cefd615785a67
cristianesilvanunes <eebmp.csn@educacao.arcos.mg.gov.br>
2024-06-03 22:21:21 -0300

    0305 aula

--- contadorImpar.java
+++ contadorImpar.java
@@ -1,9 +1,9 @@
 public class contadorImpar {
     public static void main(String[] args) {
         int quantidade = 0;
-                for (int i = 1; i<100; i++) {
-                    if (i % 2 == 1) {
-                    System.out.print(" " + i + "  ||");
+                for (int contador = 1; contador <=100; contador++) {
+                    if (contador % 2 != 0) {
+                    System.out.print(contador + " - ");
                     quantidade++;
                     }
                 }
