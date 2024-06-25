59a3b45a49a5ed2eedd2234e3569a61fec45d60e
cristianesilvanunes <eebmp.csn@educacao.arcos.mg.gov.br>
2024-06-25 18:47:54 -0300

    2

---
+++ fatorial02.java
@@ -0,0 +1,21 @@
+impor java-----------;
+public class fatorial02{
+  public static void main(String[]args) {
+    Scanner sacanner = new Scanner(---------);
+
+    System.out.print("Digite um numero inteiro: ");
+    int num;
+    num =----------;
+    //complete o código abaixo para caucular o fatorial
+    int  fatorial = 1
+    for (int i = 1; i<=-----------; i++){
+        fatorial = ---------------;
+    }
+    System.out.print("o fatorial de " + num + " é: " + fatorial;");  
+    scanner.close(); 
+  }
+}
+
+        
+    
+
