0cb58b4e06c126227112234cea0231f70c2a9151
cristianesilvanunes <eebmp.csn@educacao.arcos.mg.gov.br>
2024-06-25 20:22:35 -0300

    33

--- fatorial02.java
+++ fatorial02.java
@@ -1,15 +1,15 @@
-impor java-----------;
+impor java.util.Scanner;
 public class fatorial02{
   public static void main(String[]args) {
-    Scanner sacanner = new Scanner(---------);
+    Scanner sacanner = new Scanner(System.in);
 
     System.out.print("Digite um numero inteiro: ");
     int num;
-    num =----------;
+    num = scanner .nextInt();
     //complete o código abaixo para caucular o fatorial
     int  fatorial = 1
-    for (int i = 1; i<=-----------; i++){
-        fatorial = ---------------;
+    for (int i = 1; i <= fatorial; i++){
+        fatorial = num + fatorial;
     }
     System.out.print("o fatorial de " + num + " é: " + fatorial;");  
     scanner.close(); 
